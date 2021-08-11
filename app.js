cf90b9e578dd8f635638b29575e24d685b227c31
fuaberu <kevinfabel@gmail.com>
2021-08-11 14:05:13 -0300

    correct read button funcionality

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ class Ui {
     static displayBooks () {
         const storedBooks = [
             {
-                title: "mwecw",
-                author: "dscs",
-                pages: 8,
-                read: true
+                title: "Harry Potter and the Philosopher's Stone",
+                author: "J. K. Rowling",
+                pages: 223,
+                read: false
             },
             {
-                title: "mwecw",
-                author: "dscs",
-                pages: 8,
+                title: "Moby Dick",
+                author: "Herman Melville",
+                pages: 378,
                 read: true
             }
         ];
@@ -34,16 +34,40 @@ class Ui {
         const list = document.getElementById('book-list');
 
         const row = document.createElement('tr');
-
-        row.innerHTML = `
+        if (document.getElementById('read').checked == true || book.read == true) {
+            row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.pages}</td>
-            <td>${book.read}</td>
+            <td><a href="#" class="btn btn-success btn-sm readBtn">Read</a></td>
             <td><a href="#" class="btn btn-danger btn-sm delete" >x</a></td>
-        `;
+            `;
+        } else {
+            console.log(book.read)
+            row.innerHTML = `
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td><a href="#" class="btn btn-danger btn-sm readBtn">Not Read</a></td>
+                <td><a href="#" class="btn btn-danger btn-sm delete" >x</a></td>
+            `;
+        }
 
         list.appendChild(row);
+    }
+
+    static changeReadStatus(element) {
+        if(element.classList.contains('readBtn')) {
+            if (element.classList.contains('btn-success')) {
+                element.classList.add('btn-danger')
+                element.classList.remove('btn-success')
+                element.textContent = 'Not Read';
+            } else {
+                element.classList.remove('btn-danger')
+                element.classList.add('btn-success')
+                element.textContent = 'Read';
+            }
+        }
     }
 
     static deleteBook(element) {
@@ -56,12 +80,11 @@ class Ui {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('pages').value = '';
-        document.getElementById('read').value = '';
+        document.getElementById('read').checked = false;
     }
 }
 
-
-//display boocks when page opens
+//display books when page opens
 document.addEventListener("DOMContentLoaded", Ui.displayBooks);
 
 //add books
@@ -74,20 +97,30 @@ document.querySelector('.form-input').addEventListener('submit', (event) => {
     const pages = document.getElementById('pages').value;
     const read = document.getElementById('read').value;
 
-    //new Book
-    const book = new Book(title,author,pages,read);
-
-    //add new book
-    Ui.addBookToList(book)
-
-    //clear the input field
-    Ui.clearRow()
+    if (title === '' || author === '' || pages === '') {
+        alert('please fill in all empty field.');
+    } else {
+        //new Book
+        const book = new Book(title,author,pages,read);
+    
+        //add new book
+        Ui.addBookToList(book)
+    
+        //clear the input field
+        Ui.clearRow()
+    }
 });
 
-//remove a book
+//remove a book 
 document.getElementById('book-list').addEventListener('click', (event) => {
     Ui.deleteBook(event.target);
 });
+
+//change the read option
+document.getElementById('book-list').addEventListener('click', (event) => {
+    Ui.changeReadStatus(event.target);
+});
+
 //open the form
 function openForm() {
     document.getElementById("form").classList.remove("fade-out");
